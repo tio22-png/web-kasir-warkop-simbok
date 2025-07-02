e819a6c859fa6150d8dce63b571055a3280c329c
Tio <hanggoronursetio@gmail.com>
2025-07-02 21:34:02 +0700

    db.js

--- server/db.js
+++ server/db.js
@@ -1,21 +1,10 @@
 const mysql = require('mysql2');
 
 const connection = mysql.createConnection({
-    host: process.env.DB_HOST || 'localhost',
-    user: process.env.DB_USER || 'root',
-    password: process.env.DB_PASS || '',
-    database: process.env.DB_NAME || 'kasir'
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'kasir'
 });
-
-// Coba connect kalau di local (tidak di-render)
-if (process.env.NODE_ENV !== 'production') {
-    connection.connect((err) => {
-        if (err) {
-            console.error('Database connection failed:', err.message);
-        } else {
-            console.log('Connected to MySQL');
-        }
-    });
-}
 
 module.exports = connection;
